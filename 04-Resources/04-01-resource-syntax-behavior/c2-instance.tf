5cd1b89d415db69d624731e89da04061a953b544
kashivirus <kashivirus691@gmail.com>
2023-03-15 12:47:13 +0500

    asd

--- 04-Resources/04-01-resource-syntax-behavior/c2-instance.tf
+++ 04-Resources/04-01-resource-syntax-behavior/c2-instance.tf
@@ -3,14 +3,14 @@
 
 
 resource "aws_instance" "my-ec2-vm" {
-    ami  = "ami-0f8ca728008ff5af4"
+  ami = "ami-0f8ca728008ff5af4"
 
-    instance_type= "t2.micro"
-    availability_zone  = "ap-south-1"
-  
+  instance_type     = "t2.micro"
+#   availability_zone = "ap-south-1"
+
 
 
   tags = {
-    "Name":"Web"
+    "Name" : "Web"
   }
 }
